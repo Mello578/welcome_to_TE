b382c8c0bd3539bb8de47a981f428dfdd117d962
Yakovlev Mikhail <mikhyakovlev@ozon.ru>
2022-06-05 22:30:58 +0300

    [task_1] Adding React.memo and PureComponent for a superficial comparison props and optimized rendering. Adding function for property change checks with deep inspection

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,4 +1,17 @@
-import { Component } from 'react';
+import React, { PureComponent } from 'react';
+
+// Если хотим глубоко сравнивать объекты, то устанавливаем пакет lodash
+// npm i --save lodash.isequal
+// Т.к. через строки (JSON.stringify) не будет работать сравнение для функций и вложенных объектов
+// Ну либо пишем собственную реализацию рекурсивного обхода и сравнивания свойств объектов исходя из своих данных
+// Еще можно по очереди сравнивать каждое свойство, нууууу оно такое конечно, хотя на практике чаще применяется =)
+
+// Первый вариант плюсы: быстрее в разработке, более глубокое сравнение, надежнее, т.к. уже оттестирован, многим разработчикам знаком, оптимизирован.
+// Первый вариант минусы: вес бандла
+
+// Второй вариант плюсы: вес бандла, можно подогнать под свои данные (размерность объектов), возможно увеличит скорость
+// Второй вариант минусы: долго разрабатывать, тестировать, оптимизировать, погружаться новому разработчику тяжелее
+const _ = require('lodash');
 
 type IUser = {
     name: string
@@ -9,22 +22,42 @@ type IProps = {
     user: IUser
 }
 
-// functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+/**
+ * Специальная обертка для проверки необходимости апдейта компонента при апдейте пропсов
+ *
+ * @param propsOne - Первый пропс
+ * @param propsTwo - Второй пропс
+ */
+function needUpdateByProps<T>(propsOne: T, propsTwo: T): boolean {
+    return !(_.isEqual(propsOne, propsTwo));
+}
 
 // functional component
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+const ComponentOne = React.memo(({ name, age }: IUser) => {
+    return (
     <div>
         my name is {name}, my age is {age}
     </div>
-);
+)});
+
+const FirstComponent = React.memo(ComponentOne, needUpdateByProps);
+
+// functional component
+const ComponentTwo = ({ user: { name, age } }: IProps) => {
+    return (
+    <div>
+        my name is {name}, my age is {age}
+    </div>
+)};
+
+const SecondComponent = React.memo(ComponentTwo, needUpdateByProps);
 
 // class component
-class ThirdComponent extends Component<IUser> {
+class ThirdComponent extends PureComponent<IUser> {
+    shouldComponentUpdate(nextProps: IUser): boolean {
+        return needUpdateByProps<IUser>(nextProps, this.props)
+    }
+
     render() {
         return (
             <div>
@@ -35,7 +68,11 @@ class ThirdComponent extends Component<IUser> {
 }
 
 // class component
-class FourthComponent extends Component<IProps> {
+class FourthComponent extends PureComponent<IProps> {
+    shouldComponentUpdate(nextProps: IProps): boolean {
+        return needUpdateByProps<IProps>(nextProps, this.props)
+    }
+
     render() {
         return (
             <div>
