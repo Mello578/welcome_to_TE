37c869ea165d179048aa5a198e0b02a41af864c9
Yakovlev Mikhail <mikhyakovlev@ozon.ru>
2022-06-05 23:17:30 +0300

    [task_4] Adding useImperativeHandle for forwarding ref element for a parent component

--- task_4/index.jsx
+++ task_4/index.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import React, { Component, createRef, useState } from 'react';
 
 class MainComponent extends Component {
     myRef = createRef(); // create simple ref
@@ -15,14 +15,13 @@ class MainComponent extends Component {
     }
 };
 
-class ChildComponent extends Component {
-    state = { isActive: true };
+const ChildComponent = React.forwardRef((props, ref) => {
+    const [isActive, setIsActive] = useState(true);
 
-    toggleButton = () => this.setState({ isActive: !this.state.isActive });
+    const toggleButton = () => setIsActive(prevState => !prevState);
+    React.useImperativeHandle(ref, () => ({ toggleButton }));
 
-    render() {
-        return (
-            this.state.isActive ? <div>child component</div> : null
-        );
-    }
-};
+    return (
+        isActive ? <div>child component</div> : null
+    );
+});
