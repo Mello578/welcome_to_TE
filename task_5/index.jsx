5d1fe3e133947771670d3ae17c8854067cb21bf9
Yakovlev Mikhail <mikhyakovlev@ozon.ru>
2022-06-05 23:43:45 +0300

    [task_5] Adding wrapper for optimized component

--- task_5/index.jsx
+++ task_5/index.jsx
@@ -1,48 +1,37 @@
 import { useState } from "react";
 
-export const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => {
+// Никаких оптимизаций по колбекам не добавлял, т.к. правильнее их делать в месте создания функций
+const MouseHandlerWrapper = ({ mouseEnterCallbak, children }) => {
   const [isActive, setActive] = useState(false);
 
   const mouseEnterHandler = () => {
-    setActive(true);
-    mouseEnterCallbak();
+      setActive(true);
+      mouseEnterCallbak();
   };
 
   return (
-    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+      <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+        { children }
+      </div>
+  );
+}
+
+export const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => (
+    <MouseHandlerWrapper mouseEnterCallbak={mouseEnterCallbak}>
       <img src={imgSrc} alt={imgAlt} />
-    </div>
-  );
-};
+    </MouseHandlerWrapper>
+);
 
-export const Block2 = ({ mouseEnterCallbak, content }) => {
-  const [isActive, setActive] = useState(false);
-
-  const mouseEnterHandler = () => {
-    setActive(true);
-    mouseEnterCallbak();
-  };
-
-  return (
-    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+export const Block2 = ({ mouseEnterCallbak, content }) => (
+    <MouseHandlerWrapper mouseEnterCallbak={mouseEnterCallbak}>
       <p>{content}</p>
-    </div>
-  );
-};
+    </MouseHandlerWrapper>
+);
 
-export const Block3 = ({ mouseEnterCallbak, userData }) => {
-  const [isActive, setActive] = useState(false);
-
-  const mouseEnterHandler = () => {
-    setActive(true);
-    mouseEnterCallbak();
-  };
-
-  return (
-    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+export const Block3 = ({ mouseEnterCallbak, userData }) => (
+    <MouseHandlerWrapper mouseEnterCallbak={mouseEnterCallbak}>
       <address>
         country: {userData.country}, street: {userData.street}
       </address>
-    </div>
-  );
-};
+    </MouseHandlerWrapper>
+);
